1839e42f9295fb25da573f51bae5d9736816db69
Hitkul <hitkuljangid@gmail.com>
2016-07-07 22:03:43 +0530

    back button fixed

--- app/src/main/java/com/upstairs/indianclassic/ScreenSliderLayoutActivity.java
+++ app/src/main/java/com/upstairs/indianclassic/ScreenSliderLayoutActivity.java
@@ -83,17 +83,6 @@ public class ScreenSliderLayoutActivity extends FragmentActivity {
         });
     }
 
-    @Override
-    public void onBackPressed() {
-        if (mPager.getCurrentItem() == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed();
-        } else {
-            // Otherwise, select the previous step.
-            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
-        }
-    }
 
     /**
      * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
